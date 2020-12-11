ef7c7995ff5f982cc4d3ce34d3e63c312d5dd278
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-12-10 23:15:30 -0300

    versionando dados

--- app/models/funcionario.rb
+++ app/models/funcionario.rb
@@ -1,3 +1,18 @@
 class Funcionario < ApplicationRecord
   validates :nome, presence: true
+
+  def dias_trabalhados
+    Turno.where(funcionario_id: self.id).where("EXTRACT(MONTH FROM entrada) = ?", Time.zone.now.month).count
+  end
+  
+  def valor_receber
+    salario = Remuneracao.where(funcionario_id: self.id).first.salario rescue 0
+    return 0 if salario < 0.1
+    
+    salario_dia = salario / 22
+
+    salario_hora = salario_dia / 8
+
+    self.dias_trabalhados * salario_dia
+  end
 end
