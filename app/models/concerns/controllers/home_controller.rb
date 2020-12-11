aad093d3a6113efe5ca752dcdf6493f024c94131
Danilo Aparecido dos Santos <didox_59@yahoo.com.br>
2020-12-10 23:06:10 -0300

    versionando dados

---
+++ app/models/concerns/controllers/home_controller.rb
@@ -0,0 +1,7 @@
+class HomeController < ApplicationController
+  def index
+    @qtd_funcionarios = Funcionario.count
+    @qtd_turnos = Turno.count
+    @qtd_remuneracoes = Remuneracao.count
+  end
+end
